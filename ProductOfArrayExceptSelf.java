daa3ae2d86562baf7b4816ff4066ec6326fbf57d
Manabendu-ai <rikugithub@gmail.com>
2026-01-12 16:57:31 +0530

    Problem17 : Product of Array Except Self

---
+++ ProductOfArrayExceptSelf.java
@@ -0,0 +1,17 @@
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int ans[] = new int[len];
+
+        int pre=1, post = 1;
+        for(int i =0; i<len; i++){
+            ans[i] = pre;
+            pre = nums[i]*pre;
+        }
+        for(int i=len-1;i>=0;i++){
+            ans[i] = ans[i] * post;
+            post = post * nums[i];
+        }
+        return ans;
+    }
+}

4de72643c9b5d73ab5f54ca12ddefbd1639a90a5
Manabendu-ai <rikugithub@gmail.com>
2026-01-19 22:27:12 +0530

    Problem23: Next Permutation

---
+++ NextPermutation.java
@@ -0,0 +1,36 @@
+public class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        while(i>=0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+
+        if(i>=0){
+            int j = nums.length - 1;
+            while(j>=0 && nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i+1);
+        
+    }
+
+    public void swap(int nums[], int i, int j){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public void reverse(int nums[], int i){
+        int r = nums.length-1;
+        while(i<=r){
+            int t = nums[i];
+            nums[i] = nums[r];
+            nums[r] = t;
+            i++;
+            r--;
+        }
+    }
+}

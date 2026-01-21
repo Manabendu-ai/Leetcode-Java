4bf9bc2b0eb690c7ad4df9e167724e954cad1cda
Manabendu-ai <rikugithub@gmail.com>
2026-01-21 22:18:25 +0530

    Problem26: First Occurance in a String

---
+++ FirstOccurenceInString.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class FirstOccurenceInString {
+    
+    public int strStr(String haystack, String needle) {
+        
+        int h_len = haystack.length();
+        int n_len = needle.length();
+
+        if(n_len > h_len){
+            return -1;
+        }
+
+        for(int i=0; i<=h_len-n_len;i++){
+            int j =0;
+            while(j<n_len && haystack.charAt(i+j)==needle.charAt(j)){
+                j++;
+            }
+            if(j==n_len){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
